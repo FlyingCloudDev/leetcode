21811d69bb1895c23412ebe56090afcbc253065a
GitHubestudio <176661583+GitHubestudio@users.noreply.github.com>
2025-11-07 17:46:19 -0300

    Starting Data Structure Practices

---
+++ Big_O_practices/big_o_linear_example.py
@@ -0,0 +1,14 @@
+
+example = [1,3,10,20,2,7]
+
+def linear_example(list):
+    for number in list:
+        if number == 2:
+            return True
+    return False
+
+print(linear_example(example))
+
+# Para recorrer toda la lista y asegurarnos que el numero "N = 2" está o no en la lista, nos tardaría "N tiempo". Necesitamos chequear cada numero en la lista por lo menos una vez.
+# Haciendo esta solucion O(N), osea, mientras más grande sea "N", más tiempo va a tardar el algoritmo.
+
