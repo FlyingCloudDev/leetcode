d9e3aa8bc589d265eae675839033174ff6bf7f0b
GitHubestudio <176661583+GitHubestudio@users.noreply.github.com>
2025-11-07 17:53:02 -0300

    Comment correction

--- Big_O_practices/big_o_linear_example.py
+++ Big_O_practices/big_o_linear_example.py
@@ -10,5 +10,4 @@ def linear_example(list):
 print(linear_example(example))
 
 # Para recorrer toda la lista y asegurarnos que el numero "N = 2" está o no en la lista, nos tardaría "N tiempo". Necesitamos chequear cada numero en la lista por lo menos una vez.
-# Haciendo esta solucion O(N), osea, mientras más grande sea "N", más tiempo va a tardar el algoritmo.
-
+# Haciendo que esta solucion sea "O(N)", osea, mientras más grande sea "N", más tiempo va a tardar el algoritmo.
